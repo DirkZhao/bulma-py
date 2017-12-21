38f57d9605e24230aeaa214a546c1e4ddac3f7db
DirkZhao <732015168@qq.com>
2017-12-21 15:22:47 +0800

    参数化设置

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -4,6 +4,7 @@
 # @Author  : Dirk Zhao
 import yaml
 import os
+from xlrd import open_workbook
 
 
 class YamlReader:
@@ -21,3 +22,50 @@ class YamlReader:
             with open(self.yamlf, 'rb') as f:
                 self._data = list(yaml.safe_load_all(f))
         return self._data
+
+
+class SheetTypeError(Exception):
+    pass
+
+
+class ExcelReader:
+    def __init__(self, excel, sheet=0, title_line=True):
+        if os.path.exists(excel):
+            self.excel = excel
+        else:
+            raise FileNotFoundError
+        self.sheet = sheet
+        self.title_line = title_line
+        self._data = list()
+
+
+    @property
+    def data(self):
+        if not self._data:
+            workbook = open_workbook(self.excel)
+            if type(self.sheet) not in [int, str]:
+                raise SheetTypeError('Please pass in <type int> or <type str>,not {0}').format(type(self.sheet))
+            elif type(self.sheet) == int:
+                s = workbook.sheet_by_index(self.sheet)
+            else:
+                s = workbook.sheet_by_name(self.sheet)
+
+            if self.title_line:
+                title = s.row_values(0)  #首行为title
+                for col in range(1, s.nrows):
+                    #遍历其余行，与首行组成dict，拼到data中
+                    self._data.append(dict(zip(title, s.row_values(col))))
+            else:
+                for col in range(0, s.nrows):
+                    self._data.append(s.row_values(col))
+        return self._data
+
+
+if __name__ == '__main__':
+    y = '/Users/dirkzhao/macaca/bulma-py/config/config.yml'
+    reader = YamlReader(y)
+    print(reader.data)
+
+    e = '/Users/dirkzhao/macaca/bulma-py/data/username.csv'
+    reader = ExcelReader(e, title_line=True)
+    print(reader.data)
---
+++ utils/log.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/21 下午1:56
+# @Author  : Dirk Zhao
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from utils.config import LOG_PATH
+
+
+class Logger(object):
+    def __init__(self, logger_name='framework'):
+        self.logger = logging.getLogger(logger_name)
+        logging.root.setLevel(logging.NOTSET)
+        self.log_file_name = 'test.log'
+        self.backup_count = 5
+        #日志输出级别
+        self.console_output_level = 'WARNING'
+        self.file_output_level = 'DEBUG'
+        #日志输出格式
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s' )
+
+
+    def get_logger(self):
+        if not self.logger.handlers:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(self.formatter)
+            console_handler.setLevel(self.console_output_level)
+            self.logger.addHandler(console_handler)
+            #每天重新创建一个日志文件，最多保留backup_count份
+            file_handler = TimedRotatingFileHandler(filename=LOG_PATH+self.log_file_name,
+                                                    when='D',
+                                                    interval=1,
+                                                    backupCount=self.backup_count,
+                                                    delay=True,
+                                                    encoding='utf-8')
+            file_handler.setFormatter(self.formatter)
+            file_handler.setLevel(self.file_output_level)
+            self.logger.addHandler(file_handler)
+        return self.logger
+
+
+logger = Logger().get_logger()
