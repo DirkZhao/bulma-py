bcf19971e1e141d90e5dec88c77157476f812703
DirkZhao <732015168@qq.com>
2017-12-28 17:52:32 +0800

    修改McTest1

--- testcase/McTest1.py
+++ testcase/McTest1.py
@@ -14,6 +14,8 @@ server_url = Config().get('server_url')
 
 
 class LoginTest(unittest.TestCase):
+    excel = '/Users/dirkzhao/macaca/bulma-py/data/username.xls'
+    url = 'http://manager-center.imcoming.com/passport/login'
 
     @classmethod
     def setUpClass(cls):
@@ -25,7 +27,6 @@ class LoginTest(unittest.TestCase):
         cls.driver.quit()
 
     def test_get_url(self):
-        self.driver.get_window_size(1280, 800)
         self.driver.get(self.url)
 
     def test_login(self):
@@ -33,14 +34,14 @@ class LoginTest(unittest.TestCase):
         for d in datas:
             with self.subTest(data=d):
                 # 点击下拉框
-                self.driver.elementByXPath("/html/body/div[1]/div/form/div[1]/div/div/div[1]/i").click()
-                self.driver.sleep(1000)
+                self.driver.wait_for_element('Xpath', "/html/body/div[1]/div/form/div[1]/div/div/div[1]/i").click()
+                self.driver.wait_for(1000)
                 # 选择手机验证码登录
-                self.driver.elementByXPath("/html/body/div[2]/div/div[1]/ul/li[2]").click()
-                self.driver.sleep(1000)
+                self.driver.wait_for_element('Xpath', "/html/body/div[2]/div/div[1]/ul/li[2]").click()
+                self.driver.wait_for(1000)
                 # 获取验证码
-                self.driver.elementByXPath("/html/body/div[1]/div/form/div[2]/div[1]/div/div[1]/input").sendKeys(d['user'])
-                self.driver.elementByXPath("/html/body/div[1]/div/form/div[2]/div[2]/div/div/div/button").click()
+                self.driver.wait_for_element('Xpath', "/html/body/div[1]/div/form/div[2]/div[1]/div/div[1]/input").sendKeys(d['user'])
+                self.driver.wait_for_element('Xpath', "/html/body/div[1]/div/form/div[2]/div[2]/div/div/div/button").click()
 
 
 if __name__ == '__main__':
