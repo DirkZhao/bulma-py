6bc98b70c698e27f4a1bad7dafd43f7502d14e3a
732015168@qq.com <you10call5>
2018-04-08 14:09:10 +0800

    修改项目名称

--- utils/Yaml_rw.py
+++ utils/Yaml_rw.py
@@ -3,5 +3,3 @@
 # @Time    : 2018/4/8 14:01
 # @Author  : Dirk Zhao
 import yaml
-
-
---
+++ utils/config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/8 14:07
+# @Author  : Dirk Zhao
+import os
+
+
+def base_path(base_name):
+    curdir = os.path.abspath(os.path.curdir)
+    dirlist = curdir.split(os.path.sep)
+    path = ''
+    for dir in dirlist[1:]:
+        path = path + os.path.sep + dir
+        if dir == base_name:
+            return path
+
+
+BASE_PATH = base_path('bulma_py')
+CONFIG_FILE = os.path.join(BASE_PATH, 'config', 'test.ini')
+DATA_PATH = os.path.join(BASE_PATH, 'data')
+DRIVER_PATH = os.path.join(BASE_PATH, 'drivers')
+LOG_PATH = os.path.join(BASE_PATH, 'log')
+REPORT_PATH = os.path.join(BASE_PATH, 'report')
